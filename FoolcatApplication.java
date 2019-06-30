b39792fb2e894755f5d6f3bd3c8ba60e01140bc3
LeoJian622 <hualuoyu_yks@qq.com>
2019-06-30 15:33:12 +0800

    Add files via upload

---
+++ FoolcatApplication.java
@@ -0,0 +1,13 @@
+package xyz.foolcat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoolcatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoolcatApplication.class, args);
+	}
+
+}
