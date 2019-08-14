fd14bc30983bf38d3af81542907f7fc7c080d531
Richard <hualuoyu_yks@qq.com>
2019-08-14 17:29:14 +0800

    解密

--- src/main/java/xyz/foolcat/controller/WeChatAuthReturnController.java
+++ src/main/java/xyz/foolcat/controller/WeChatAuthReturnController.java
@@ -3,19 +3,22 @@ package xyz.foolcat.controller;
 import com.alibaba.fastjson.JSON;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.codec.digest.DigestUtils;
-import org.apache.commons.lang3.ObjectUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 import xyz.foolcat.config.WeChatAuthenticateConfig;
 import xyz.foolcat.dto.WeChatAuthDTO;
 import xyz.foolcat.dto.WeChatAuthReturnDTO;
-import xyz.foolcat.mapper.UserInfoMapper;
+import xyz.foolcat.exception.LoginException;
 import xyz.foolcat.model.UserInfo;
 import xyz.foolcat.service.UserInfoService;
+import xyz.foolcat.utils.AesEncryptUtils;
 import xyz.foolcat.utils.JwtUtils;
 
 import java.net.URI;
@@ -42,16 +45,15 @@ public class WeChatAuthReturnController {
     private final UserInfoService userInfoService;
 
     @Autowired
-    public WeChatAuthReturnController(WeChatAuthenticateConfig weChatAuthenticateConfig, JwtUtils jwtUtils, RestTemplate restTemplate, RedisTemplate redisTemplate, UserInfoService userInfoService){
+    public WeChatAuthReturnController(WeChatAuthenticateConfig weChatAuthenticateConfig, JwtUtils jwtUtils, RestTemplate restTemplate, RedisTemplate redisTemplate, UserInfoService userInfoService) {
         this.weChatAuthenticateConfig = weChatAuthenticateConfig;
         this.restTemplate = restTemplate;
         this.redisTemplate = redisTemplate;
         this.jwtUtils = jwtUtils;
         this.userInfoService = userInfoService;
-    };
+    }
 
-
-    @RequestMapping(value = "/authentication", method = RequestMethod.POST)
+    @PostMapping(value = "/authentication", produces = "application/json")
     public String authenticate(@RequestBody WeChatAuthDTO weChatAuthDTO) throws Exception {
 
         URI url = UriComponentsBuilder.fromUriString(weChatAuthenticateConfig.getApiUrl()
@@ -62,23 +64,32 @@ public class WeChatAuthReturnController {
         WeChatAuthReturnDTO weChatAuthReturnDTO = JSON.parseObject(responseEntity.getBody(), WeChatAuthReturnDTO.class);
         log.debug("code请求返回：{}", responseEntity.getBody());
         int errorCode = weChatAuthReturnDTO.getErrcode();
-        System.out.println();
         if (0 != errorCode) {
             log.error("获取微信SEESION_KEY失败 errcode:{} errmsg:{}", weChatAuthReturnDTO.getErrcode(), weChatAuthReturnDTO.getErrmsg());
             return JSON.toJSONString(weChatAuthReturnDTO);
         } else {
-            redisTemplate.opsForValue().set(DigestUtils.md5Hex(weChatAuthReturnDTO.getOpenId()), weChatAuthReturnDTO);
-            log.info("获取微信SEESION_KEY成功");
+
             UserInfo userInfo = userInfoService.getByUnionId(weChatAuthReturnDTO.getOpenId());
 
-            if (null == userInfo){
-                userInfo =  new UserInfo()
-                        .withUnionId(weChatAuthReturnDTO.getOpenId());
+            if (null == userInfo) {
+                /**
+                 * 数据解密
+                 */
+                byte[] decryptdata = AesEncryptUtils.decrypt(weChatAuthDTO.getEncryptData(), weChatAuthReturnDTO.getSessionKey(), weChatAuthDTO.getIv());
+                if (decryptdata == null) {
+                    throw new LoginException("获取不到用户信息");
+                }
+
+                userInfo = JSON.parseObject(new String(decryptdata), UserInfo.class);
                 userInfoService.insert(userInfo);
             }
+
+            redisTemplate.opsForValue().set(DigestUtils.md5Hex(weChatAuthReturnDTO.getOpenId()), weChatAuthReturnDTO);
+            log.info("获取微信SEESION_KEY成功");
+
             //生成token并返回
             String token = jwtUtils.generateToken(userInfo);
-            System.out.println(token);
+            log.debug("token：{}", token);
             return token;
         }
     }
---
+++ src/main/java/xyz/foolcat/exception/LoginException.java
@@ -0,0 +1,14 @@
+package xyz.foolcat.exception;
+
+/**
+ * 登录异常
+ *
+ * @author liyongjian
+ * @create 2019-08-14 16:54
+ */
+
+public class LoginException extends Exception{
+    public LoginException(String message){
+        super(message);
+    }
+}
